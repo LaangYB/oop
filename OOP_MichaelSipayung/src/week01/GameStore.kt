92741a6439461bb0ab36d43e93713b5e02ce8228
unknown <sedekgaming123@gmail.com>
2026-04-19 11:33:37 +0700

    week01:step13(tugas2)

--- OOP_MichaelSipayung/src/week01/GameStore.kt
+++ OOP_MichaelSipayung/src/week01/GameStore.kt
@@ -4,22 +4,28 @@ fun main() {
     val gameTitle = "Black Myth: Wukong"
     val price = 700000
 
+    val userNote: String? = null
+
     val discount = calculateDiscount(price = price)
     val finalPrice = price - discount
 
 
     printReceipt(
         title = gameTitle,
-        finalPrice = finalPrice
+        finalPrice = finalPrice,
+        note = userNote
     )
 }
 
 fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
-
-fun printReceipt(title: String, finalPrice: Int) {
+fun printReceipt(title: String, finalPrice: Int, note: String?) {
     println("=== SteamKW Game Receipt ===")
     println("Judul Game  : $title")
     println("Harga Akhir : Rp $finalPrice")
+
+
+    println("Catatan     : ${note ?: "Tidak ada catatan"}")
+
     println("============================")
 }
