f10ba8c14ca05be5053a58a2b5180746477db7b0
unknown <sedekgaming123@gmail.com>
2026-04-19 20:39:12 +0700

    week03:step05

--- OOP_MichaelSipayung/src/week03/main.kt
+++ OOP_MichaelSipayung/src/week03/main.kt
@@ -2,6 +2,12 @@ package week03
 
 fun main(){
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("gaji: ${e.salary}")
+
+    e.increasePerformanceRating()
+
+    println("pajak yang harus dibayar: ${e.tax}")
 }
