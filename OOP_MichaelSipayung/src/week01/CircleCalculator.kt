da5c2dee9380fa6803b060522e517655768e78f4
unknown <sedekgaming123@gmail.com>
2026-04-19 11:26:45 +0700

    week01:step07(tugas1)

--- OOP_MichaelSipayung/src/week01/CircleCalculator.kt
+++ OOP_MichaelSipayung/src/week01/CircleCalculator.kt
@@ -1,18 +1,21 @@
 package week01
 
-fun main(args: Array<String>){
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+fun main(args: Array<String>) {
+    // Langkah 2: Menggunakan val dan Type Inference
+    val radius = 7.0
+    val pi = 3.14
 
-    var area: Double = pi * radius * radius
-    println("Radius: " + radius + ", Area: " + area)
+    val area = pi * radius * radius
 
-    //logic check
+    // Menggunakan String Template
+    println("Radius: $radius, Area: $area")
+
+    // Logic check
     checkSize(area)
-
 }
+
 fun checkSize(area: Double) {
-    if (area > 100){
+    if (area > 100) {
         println("This is a Big circle")
     } else {
         println("This is a Small circle")
