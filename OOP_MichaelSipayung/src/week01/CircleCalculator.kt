0e2050115a02101226c04ff50d4755a17ec9c3d5
unknown <sedekgaming123@gmail.com>
2026-04-19 11:28:01 +0700

    week01:step09(tugas1)

--- OOP_MichaelSipayung/src/week01/CircleCalculator.kt
+++ OOP_MichaelSipayung/src/week01/CircleCalculator.kt
@@ -1,15 +1,13 @@
 package week01
 
-fun main(args: Array<String>) {
+fun main() {
     val radius = 7.0
     val pi = 3.14
 
     val area = pi * radius * radius
     println("Radius: $radius, Area: $area")
 
-    // Memanggil fungsi di dalam println sesuai instruksi
     println(checkSize(area))
 }
 
-// Menggunakan Expression Body dan 'if' sebagai expression
 fun checkSize(area: Double) = if (area > 100) "This is a Big circle" else "This is a Small circle"
