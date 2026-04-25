98dfe48d055391a6eca7eccf07186f39d72524be
unknown <sedekgaming123@gmail.com>
2026-04-25 13:48:54 +0700

    week04:step11(task2)

--- OOP_MichaelSipayung/src/week04/Developer.kt
+++ OOP_MichaelSipayung/src/week04/Developer.kt
@@ -1,2 +1,17 @@
 package week04
 
+// Developer mewarisi Employee dan menambah parameter programmingLanguage
+class Developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : Employee(name, baseSalary) {
+
+    // Override work untuk menampilkan bahasa pemrograman yang digunakan
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+
+    // calculateBonus tidak ditulis di sini karena
+    // secara otomatis menggunakan implementasi dari Parent (Employee)
+}
--- OOP_MichaelSipayung/src/week04/Main.kt
+++ OOP_MichaelSipayung/src/week04/Main.kt
@@ -7,13 +7,29 @@ fun main() {
     generalVehicle.accelerate()
 
     println("\n--- Testing Car ---")
-    val myCar = Car(brand = "Toyota", numberofDoors = 4)
+    // Perbaikan: Pastikan nama parameter sesuai dengan yang ada di class Car (numberOfDoors)
+    val myCar = Car(brand = "Toyota",4)
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
 
-    val myEv = ElectricCar("Tesla Model 3", 4,90)
+    println("\n--- Testing Electric Car ---")
+    val myEv = ElectricCar("Tesla Model 3", 4, 90)
     myEv.openTrunk()
-    myEv.accelerate()
+    myEv.accelerate() // Ini akan mencetak "berakselerasi dalam sunyi"
     myEv.honk()
+
+    println("\n--- Data Manager ---")
+    // Perbaikan: Panggil 'Manager', bukan 'Manager.kt'
+    val manager = Manager("Budi", 10000000)
+    manager.work()
+    // Mencetak bonus: (10% * 10jt) + 500rb = 1.500.000
+    println("Bonus ${manager.name}: Rp ${manager.calculateBonus()}")
+
+    println("\n--- Data Developer ---")
+    // 2. Instansiasi objek Developer
+    val developer = Developer("Andi", 8000000, "Kotlin")
+    developer.work()
+    // Mencetak bonus: (10% * 8jt) = 800.000 (menggunakan logika Employee)
+    println("Bonus ${developer.name}: Rp ${developer.calculateBonus()}")
 }
