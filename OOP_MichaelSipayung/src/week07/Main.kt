0ca85a6a2a11e7f742d5763c4f239d11a70ffa5c
unknown <sedekgaming123@gmail.com>
2026-04-29 21:31:12 +0700

    week07:step10

--- OOP_MichaelSipayung/src/week07/Main.kt
+++ OOP_MichaelSipayung/src/week07/Main.kt
@@ -4,6 +4,7 @@ fun main() {
     println("=== TEST SINGLETON ===")
     println("Status: ${DatabaseManager.connectionStatus}")
     DatabaseManager.connect()
+    println("Status Terbaru: ${DatabaseManager.connectionStatus}")
 
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient()
@@ -12,18 +13,28 @@ fun main() {
     println("\n=== TEST REGULAR CLASS ===")
     val reg1 = RegularUser("Alice", 28)
     val reg2 = RegularUser("Alice", 28)
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
+    println(reg1) // Akan mencetak alamat memori (hashcode)
+    println("Sama? ${reg1 == reg2}") // False, karena referensi memori berbeda
 
     println("\n=== TEST DATA CLASS ===")
     val data1 = DataUser("Alice", 28)
     val data2 = DataUser("Alice", 28)
-    println(data1)
-    println("Sama? ${data1 == data2}")
+    println(data1) // Akan mencetak konten: DataUser(name=Alice, age=28)
+    println("Sama? ${data1 == data2}") // True, karena kontennya sama
 
     val data3 = data1.copy(age = 23)
     println("Hasil Copy: $data3")
 
-    val(userName, userAge) = data1
+    val (userName, userAge) = data1 // Destructuring Declarations
     println("Destructured: $userName berumur $userAge")
+
+    println("\n=== TEST SEALED CLASS ===")
+    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+
+    val uiMessage = when(response) {
+        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        // Tidak butuh 'else' karena sealed class sudah mencakup semua kemungkinan
+    }
+    println(uiMessage)
 }
