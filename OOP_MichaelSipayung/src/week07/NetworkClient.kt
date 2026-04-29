92cbc50db338363661d01e58e25ff9f16090eb2a
unknown <sedekgaming123@gmail.com>
2026-04-29 21:08:37 +0700

    week07:step03

--- OOP_MichaelSipayung/src/week07/NetworkClient.kt
+++ OOP_MichaelSipayung/src/week07/NetworkClient.kt
@@ -1,2 +1,16 @@
 package week07
 
+class NetworkClient private constructor(val url: String) {
+   companion object {
+       const val BASE_URL = "http://api.umn.ac.id"
+
+       fun createClient(): NetworkClient {
+           println("Membangun NetworkClient dengan Base_URL: $BASE_URL")
+           return NetworkClient("$BASE_URL")
+       }
+   }
+
+    fun connect() {
+        println("Connecting... to $url")
+    }
+}
