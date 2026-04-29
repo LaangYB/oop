8bc861a2ba95e91f51e41e8cd015ef8a9353217c
unknown <sedekgaming123@gmail.com>
2026-04-29 20:26:59 +0700

    week06:step03

--- OOP_MichaelSipayung/src/week06/Clickable.kt
+++ OOP_MichaelSipayung/src/week06/Clickable.kt
@@ -4,3 +4,8 @@ interface Clickable{
     val name: String
     fun click()
 }
+class Button(override val name: String) : Clickable{
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!")
+    }
+}
