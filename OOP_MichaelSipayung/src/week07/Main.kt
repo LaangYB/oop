940dff070955c0428159e9d03920f64eafedcd36
unknown <sedekgaming123@gmail.com>
2026-04-29 21:16:04 +0700

    week07:step06

--- OOP_MichaelSipayung/src/week07/Main.kt
+++ OOP_MichaelSipayung/src/week07/Main.kt
@@ -1,2 +1,23 @@
 package week07
 
+fun main() {
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Alice", 28)
+    val reg2 = RegularUser("Alice", 28)
+    println(reg1)
+    println("Sama? ${reg1 == reg2}")
+
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 28)
+    val data2 = DataUser("Alice", 28)
+    println(data1)
+    println("Sama? ${data1 == data2}")
+}
