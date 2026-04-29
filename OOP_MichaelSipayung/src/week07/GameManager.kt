a48f4b435da16a50ec51bcdf870eb5d33291c659
unknown <sedekgaming123@gmail.com>
2026-04-29 21:41:42 +0700

    week07:step14(task)

--- OOP_MichaelSipayung/src/week07/GameManager.kt
+++ OOP_MichaelSipayung/src/week07/GameManager.kt
@@ -1,2 +1,20 @@
 package week07
 
+object GameManager {
+
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine... Selamat datang di dunia RPG!")
+        }
+    }
+
+    fun stopGame() {
+        isGameRunning = false
+        println("Game Engine dihentikan.")
+    }
+}
--- OOP_MichaelSipayung/src/week07/GameModels.kt
+++ OOP_MichaelSipayung/src/week07/GameModels.kt
@@ -1,2 +1,11 @@
 package week07
 
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70), UNCOMMON(40), RARE(15), EPIC(5), LEGENDARY(1)
+}
+
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
