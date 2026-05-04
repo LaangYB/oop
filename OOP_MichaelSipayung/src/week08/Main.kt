1fa6cf086e3b3dafcfe324bf0da8afe4fb1a7690
unknown <sedekgaming123@gmail.com>
2026-05-04 23:12:17 +0700

    week08:step05

--- OOP_MichaelSipayung/src/week08/Main.kt
+++ OOP_MichaelSipayung/src/week08/Main.kt
@@ -1,2 +1,20 @@
 package week08
 
+fun main(){
+    println("=== TEST LET BLOCK ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota " +
+    "tidak diketahui"
+    println("Tujuan pengiriman: $destination")
+
+    println("=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 2500000)
+    val receipt = validOrder.totalPrice?.let { price ->
+
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
+}
--- OOP_MichaelSipayung/src/week08/NotificationService.kt
+++ OOP_MichaelSipayung/src/week08/NotificationService.kt
@@ -1,2 +1,15 @@
 package week08
 
+class NotificationService {
+    fun sendEmail(emailAddress: String){
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile){
+        if (user.email != null){
+            sendEmail(user.email!!)
+        } else {
+            println("User ${user.name} tidak memiliki email")
+        }
+    }
+}
